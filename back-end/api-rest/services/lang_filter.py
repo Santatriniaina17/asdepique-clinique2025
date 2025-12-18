5596bf152bf669f5f40d3faa93ddcf12786bc26e
Santatriniaina17 <dilan.santatra2000@gmail.com>
2025-12-18 16:11:34 +0300

    beforedep

--- back-end/api-rest/services/lang_filter.py
+++ back-end/api-rest/services/lang_filter.py
@@ -5,7 +5,7 @@ FRENCH_SUFFIXES = (
 )
 
 INVALID_MG_PATTERNS = (
-    "nb", "mk", "dt", "bp", "sz","th", "sn", "nl","dn"
+    "nb", "mk", "dt", "bp", "sz","th", "sn", "nl","dn","nf"
 )
 
 def is_malagasy_like(word):
